239953cd12cb724a54d652cba4b0825ef76a47f6
rGharco <gabrielradulescu05@protonmail.com>
2026-01-14 14:01:54 +0200

    revert la modificari pentru deployment

--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ const { sequelize, User } = require('./models'); // folosim User ca sa hardcodam
 const userRoutes = require('./routes/users');
 const taskRoutes = require('./routes/tasks');
 const subtaskRoutes = require('./routes/subtasks');
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,11 +17,6 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client/taskplanner/dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/taskplanner/dist', 'index.html'));
-});
 
 // Routes
 app.use('/api/users', userRoutes);
