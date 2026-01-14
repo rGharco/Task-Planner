7c4908ca691e41c286a56f0a48ba0d12045b2b0a
Visan Alexandru-Teodor <visanalexandru23@stud.ase.ro>
2026-01-14 15:10:15 +0200

    changed default port for backend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const taskRoutes = require('./routes/tasks');
 const subtaskRoutes = require('./routes/subtasks');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
